e50681395fc14349980c81f69b803d8b67f22cd8
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-02-08 23:08:36 +0600

    Removed empty spaces.

--- HW10/ViewController.swift
+++ HW10/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  HW10
-//
-//  Created by Kuanysh al-Khattab Auyelgazy on 08.02.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
