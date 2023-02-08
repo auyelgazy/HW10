08a316a5259c7d9b104a2cff11e547d019075048
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-02-08 23:02:09 +0600

    Initial Commit

---
+++ HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
